3437569d222053f04329eb28379241ab7b7b8e6d
Struan King <struan.king@gmail.com>
2020-06-08 15:23:36 +0100

    Adds service-worker phase 1

---
+++ src/site/_includes/js/service-worker.js
@@ -0,0 +1,28 @@
+var cacheName = "v2";
+var cacheList = ["/", "index.html", "css/things.css"]; //, "js/color-mode.js"];
+
+self.addEventListener("install", function (event) {
+  console.log("SW: Install");
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(cacheList);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("SW: Activate");
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log("SW: checking for cached asset");
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        console.log("SW: Using cached asset");
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
