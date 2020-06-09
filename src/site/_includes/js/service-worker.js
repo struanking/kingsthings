7401251b05919af8f292d946098f3c913cc2c35c
Struan King <struan.king@gmail.com>
2020-06-09 15:33:29 +0100

    Debug sw

--- src/site/_includes/js/service-worker.js
+++ src/site/_includes/js/service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = "kingsthings_v2";
+var cacheName = "kingsthings_v3";
 var cacheList = ["/", "index.html", "css/things.css"]; //, "js/color-mode.js"];
 
 self.addEventListener("install", function (event) {
@@ -18,12 +18,15 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.open(cacheName).then(function (cache) {
       return cache.match(event.request).then(function (response) {
+        console.log("event.request", event.request);
         return (
           response ||
           fetch(event.request).then(function (response) {
             if (/\.jpg$|.png$|.webp$/.test(event.request.url)) {
-              console.log("Cache image", event.request.url);
+              console.log("Cache asset", event.request.url);
               cache.put(event.request, response.clone());
+            } else {
+              console.log("Do not cache asset", event.request.url);
             }
             return response;
           })
