d033b5b5942a7a0fd88222e0cc37e096fec6ecc7
Struan King <struan.king@gmail.com>
2020-07-07 16:29:08 +0100

    Adds font-display swap

--- gulp-tasks/fonts.js
+++ gulp-tasks/fonts.js
@@ -11,7 +11,7 @@ const fonts = async () => {
 
   // Grabs fonts and CSS from google and puts in the dist folder
   const result = await instance.download(
-    "https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500"
+    "https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap"
   );
 
   return result;
